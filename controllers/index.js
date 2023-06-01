452b6be63ba062ae37c767d29752caa2dd0fb9d2
Solomon K <95310414+SoloGoblin@users.noreply.github.com>
2023-06-01 13:26:29 -0400

    aight

--- controllers/index.js
+++ controllers/index.js
@@ -3,10 +3,19 @@ router = express.Router();
 const fs = require('fs');
 
 
+router.get("/unauth/",(request,response)=>{
+    response.status(401);
+    response.setHeader('Content-Type', 'text/html')
+    response.render("unauth",{
+      user:request.user
+    }) 
+  
+  })
+
 router.get('/', function(request, response) {
     response.status(200);
     response.setHeader('Content-Type', 'text/html')
-    response.render("index", {
+    response.render("unauth", {
         user: request.user
     });
 });
