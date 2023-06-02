2883052aa987d6f7a299b10ec1e3bca05590f9ae
varun-gupta24 <varun.gupta24@trinityschoolnyc.org>
2023-06-02 12:15:41 -0400

    p

--- controllers/index.js
+++ controllers/index.js
@@ -3,19 +3,20 @@ router = express.Router();
 const fs = require('fs');
 
 
-router.get("/unauth/",(request,response)=>{
-    response.status(401);
-    response.setHeader('Content-Type', 'text/html')
-    response.render("unauth",{
-      user:request.user
-    }) 
-  
-  })
 
 router.get('/', function(request, response) {
     response.status(200);
     response.setHeader('Content-Type', 'text/html')
-    response.render("unauth", {
+    response.render("login", {
+        user: request.user
+    });
+});
+
+
+router.get('/index', function(request, response) {
+    response.status(200);
+    response.setHeader('Content-Type', 'text/html')
+    response.render("index", {
         user: request.user
     });
 });
@@ -28,5 +29,6 @@ router.get('/login', function(request, response) {
         user: request.user
     });
 });
+
 
 module.exports=router;
